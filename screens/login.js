4c0d43d0b734418fe1ea5b1151959cbbffab3c11
Renato Matta Machado <renatommps@gmail.com>
2021-02-08 23:09:22 -0300

    Update and add reference code on login page

--- screens/login.js
+++ screens/login.js
@@ -1,61 +1,89 @@
 import React from 'react';
-import { Image, StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { AsyncStorage, Image, StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 import { CommonActions, NavigationContainer } from '@react-navigation/native';
 
 export default class Login extends React.Component {
-  state = {
-    email: "",
-    password: ""
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
+
+  async storeToken(user) {
+    try {
+       await AsyncStorage.setItem("userData", JSON.stringify(user));
+    } catch (error) {
+      console.log("Something went wrong", error);
+    }
+  }
+
+  async getToken(user) {
+    try {
+      let userData = await AsyncStorage.getItem("userData");
+      let data = JSON.parse(userData);
+      console.log(data);
+    } catch (error) {
+      console.log("Something went wrong", error);
+    }
+  }
+
+  handleEmail = text => {
+    this.setState({ email: text });
+  };
+
+  handlePassword = text => {
+    this.setState({ password: text });
+  };
+
+  login = (email, pass) => {
+    this.props.navigation.dispatch(
+      CommonActions.reset({
+        index: 0,
+        routes: [
+          { name: 'Home' },
+        ],
+      })
+    )
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Image style={styles.logo} source={require('../assets/logo-placeholder.png')}/>
-        <View style={styles.inputView} >
+        <Image style={styles.logo} resizeMode="center" source={require('../assets/logo.png')}/>
+        <View style={styles.inputView}>
           <TextInput
             style={styles.inputText}
             placeholder="Email..."
             placeholderTextColor="#003f5c"
-            onChangeText={text => this.setState({email:text})}/>
+            autoCapitalize="none"
+            onChangeText={this.handleEmail}
+          />
         </View>
-        <View style={styles.inputView} >
+        <View style={styles.inputView}>
           <TextInput
-            secureTextEntry
+            secureTextEntry={true}
             style={styles.inputText}
             placeholder="senha..."
             placeholderTextColor="#003f5c"
-            onChangeText={text => this.setState({password:text})}/>
+            autoCapitalize="none"
+            onChangeText={this.handlePassword}
+          />
         </View>
         <TouchableOpacity>
           <Text style={styles.forgot}>Esqueceu a senha?</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.loginBtn}>
+        <TouchableOpacity style={styles.loginBtn} onPress={() => this.login(this.state.email, this.state.password)}>
           <Text style={styles.loginText}>Logar</Text>
         </TouchableOpacity>
         <TouchableOpacity>
-          <Text style={styles.loginText}>Cadastrar</Text>
+          <Text style={styles.subscribeText}>Cadastrar</Text>
         </TouchableOpacity>
       </View>
     );
   }
 }
-
-{/* <View style={styles.container}>
-  <Text>This is the login screen!</Text>
-  <Button
-    title='Go to Home screen'
-    onPress={() =>
-      this.props.navigation.dispatch(
-        CommonActions.reset({
-          index: 0,
-          routes: [
-            { name: 'Home' },
-          ],
-        })
-      )}
-  />
-</View> */}
 
 const styles = StyleSheet.create({
   container: {
@@ -65,7 +93,9 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   logo: {
-    marginBottom: 40
+    width: 300,
+    height: 300,
+    marginBottom: 0
   },
   inputView: {
     width: "80%",
@@ -86,7 +116,7 @@ const styles = StyleSheet.create({
   },
   loginBtn:{
     width: "80%",
-    backgroundColor: "#fb5b5a",
+    backgroundColor: "#edc951",
     borderRadius: 25,
     height: 50,
     alignItems: "center",
@@ -95,6 +125,9 @@ const styles = StyleSheet.create({
     marginBottom: 10
   },
   loginText: {
+    color: "#343434"
+  },
+  subscribeText: {
     color: "white"
   }
 });
