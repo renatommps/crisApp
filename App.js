91e8bfd062d2b56ee5cf7a872efd77260db2cdc6
Renato Matta Machado <renatommps@gmail.com>
2021-02-08 23:11:49 -0300

    Use defined navigators

--- App.js
+++ App.js
@@ -1,48 +1,35 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { createStackNavigator } from '@react-navigation/stack';
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
-import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { NavigationContainer } from "@react-navigation/native";
+import MainStackNavigator from "./navigation/stackNavigator";
+import BottomTabNavigator from "./navigation/tabNavigator";
+
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createDrawerNavigator } from '@react-navigation/drawer';
+// import { createStackNavigator } from '@react-navigation/stack';
+// import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+// import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import { Image } from 'react-native';
 import Login from './screens/login';
 import Home from './screens/home';
 
-const Drawer = createDrawerNavigator();
-const Stack = createStackNavigator();
-const MaterialBottomTabs = createMaterialBottomTabNavigator();
-const MaterialTopTabs = createMaterialTopTabNavigator();
+// const Drawer = createDrawerNavigator();
+// const Stack = createStackNavigator();
+// const MaterialBottomTabs = createMaterialBottomTabNavigator();
+// const MaterialTopTabs = createMaterialTopTabNavigator();
 
 export default class App extends React.Component {
 
-  createDrawer = () =>
-    <Drawer.Navigator>
-      <Stack.Screen name="Login" component={Login}/>
-      <Drawer.Screen name="Home" component={Home}/>
-    </Drawer.Navigator>
+  // createDrawer = () =>
+  //   <Drawer.Navigator>
+  //     <Stack.Screen name="Login" component={Login}/>
+  //     <Drawer.Screen name="Home" component={Home}/>
+  //   </Drawer.Navigator>
 
   render() {
     return (
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Login" component={Login}
-            options={{headerShown: false}}
-          />
-          <Stack.Screen name="Home" component={Home}
-            options = {{
-              headerTransparent:true,
-              headerLeft : () => (
-                <Image source={require('./assets/favicon.png')}/>
-              ),
-              title : '',
-              headerRight : () => (
-                <Image source={require('./assets/favicon.png')} style={{marginHorizontal: 20, marginTop: 40}}/>
-              )
-            }}
-          />
-        </Stack.Navigator>
+        <MainStackNavigator/>
       </NavigationContainer>
     );
   }
 }
-
