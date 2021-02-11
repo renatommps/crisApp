89c64cb9622ea9f3a7710a812a571c01160f9577
Renato Matta Machado <renatommps@gmail.com>
2021-02-11 10:20:06 -0300

    Adjust icons

--- src/navigation/tabNavigator.js
+++ src/navigation/tabNavigator.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { View } from "react-native";
 import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import Quiz from "../screens/Quiz";
@@ -10,26 +11,36 @@ const Tab = createMaterialBottomTabNavigator();
 const BottomTabNavigator = () => {
   return (
     <Tab.Navigator
+      labeled={false}
       initialRouteName="Home"
+      activeColor="#EDC951"
+      inactiveColor="#6C6C6C"
       barStyle={{
         backgroundColor: '#ffffff'
       }}
     >
       <Tab.Screen
-        name="Quiz"
+        name="Jogar"
         component={Quiz}
         options={{
-          tabBarIcon: () => (
-            <MaterialCommunityIcons name="play-circle-outline" color={'#343434'} size={26}  />
-          ),
+          tabBarIcon: ({ color }) => (
+            <View style={{position:'absolute', top: -5, right: 0, bottom: 0, left: 0, height: 100, width: 100, padding: 0, margin: 0}}>
+              <MaterialCommunityIcons name="play-circle-outline" color={color} size={45}
+              style={{height: '100%', width: '100%', padding: 0, margin: 0, top: 0, right: 0, bottom: 0, left: 0 }}/>
+            </View>
+          )
         }}
       />
       <Tab.Screen
         name="Home"
         component={Home}
         options={{
-          tabBarIcon: () => (
-            <MaterialCommunityIcons name="home-outline" color={'#343434'} size={26} />
+          tabBarIcon: ({ color }) => (
+            <View style={{position:'absolute', top: -5, right: 0, bottom: 0, left: 0, height: 100, width: 100, padding: 0, margin: 0}}>
+              <MaterialCommunityIcons name="home-outline" color={color} size={45}
+                style={{height: '100%', width: '100%', padding: 0, margin: 0, top: 0, right: 0, bottom: 0, left: 0 }}
+              />
+            </View>
           )
         }}
       />
@@ -37,9 +48,13 @@ const BottomTabNavigator = () => {
         name="Perfil"
         component={Profile}
         options={{
-          tabBarIcon: () => (
-            <MaterialCommunityIcons name="account-circle-outline" color={'#343434'} size={26} />
-          ),
+          tabBarIcon: ({ color }) => (
+            <View style={{position:'absolute', top: -5, right: 0, bottom: 0, left: 0, height: 100, width: 100, padding: 0, margin: 0}}>
+              <MaterialCommunityIcons name="account-circle-outline" color={color} size={45}
+                style={{height: '100%', width: '100%', padding: 0, margin: 0, top: 0, right: 0, bottom: 0, left: 0 }}
+              />
+            </View>
+          )
         }}
       />
     </Tab.Navigator>
