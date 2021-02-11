d7abe2b1cb8b47553029c43079a9aa65ef5fe736
Renato Matta Machado <renatommps@gmail.com>
2021-02-11 18:18:28 -0300

    Implemente login sreen layout

--- src/screens/Profile/index.js
+++ src/screens/Profile/index.js
@@ -1,10 +1,27 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 import { styles } from "./styles";
+import KnowledgeArea from "../KnowledgeArea";
+import TalkToCris from "../TalkToCris";
+
 const Profile = () => {
+  const navigation = useNavigation();
+
   return (
     <View style={styles.container}>
-      <Text>This is the Profile screen!</Text>
+      <View style={styles.topContainer}>
+      <Text style={styles.pontuationText}>Sua pontuação</Text>
+      <Text style={styles.pontuationValue}>17pts</Text>
+      </View>
+      <View style={styles.bottomContainer}>
+        <TouchableOpacity style={styles.btn} onPress={() => navigation.navigate("KnowledgeArea")}>
+          <Text style={styles.bottomText}>Área do Conhecimento</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.btn} onPress={() => navigation.navigate("TalkToCris")}>
+          <Text style={styles.bottomText}>#FaleComOCris</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
