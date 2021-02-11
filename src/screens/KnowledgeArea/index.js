9014d43c48705e5d010ea53e9838bb8dd1c1ac45
Renato Matta Machado <renatommps@gmail.com>
2021-02-11 19:37:15 -0300

    Add knowledgeArea implementation

--- src/screens/KnowledgeArea/index.js
+++ src/screens/KnowledgeArea/index.js
@@ -1,12 +1,23 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { ScrollView, Text, View } from "react-native";
 import { styles } from "./styles";
+import article from './article.js';
 
+const myArticle = article;
 export default class KnowledgeArea extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>This is the KnowledgeArea screen!</Text>
+        <View style={styles.titleContainer}>
+          <Text style={styles.titleText}>Código de Trânsito Brasileiro</Text>
+        </View>
+        <View style={styles.textContainer}>
+         <ScrollView>
+          <Text style={styles.textContent}>
+            {myArticle}
+          </Text>
+         </ScrollView>
+        </View>
       </View>
     );
   }
--- src/screens/KnowledgeArea/styles.js
+++ src/screens/KnowledgeArea/styles.js
@@ -2,8 +2,31 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#6C6C6C",
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "center"
   },
+  titleContainer: {
+    flex: 2,
+    alignItems: "flex-start",
+    justifyContent: "flex-end",
+    width: "86%",
+    marginBottom: 20
+  },
+  titleText: {
+    color: "#FFFFFF",
+    fontSize: 17,
+    textAlign: "left",
+    fontWeight: "bold"
+  },
+  textContainer: {
+    flex: 9,
+    backgroundColor: "#EDC951",
+    width: "90%",
+    padding: 15
+  },
+  textContent: {
+    color: "#707070",
+    fontSize: 20
+  }
 });
