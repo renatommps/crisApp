b317fceb8a42e6c8169a73a04d1c44c944e2c70c
Renato Matta Machado <renatommps@gmail.com>
2021-02-10 21:46:20 -0300

    Fix app location

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import MainStackNavigator from "./navigation/stackNavigator";
+import MainStackNavigator from "./src/navigation/stackNavigator";
 
 // import { NavigationContainer } from '@react-navigation/native';
 // import { createDrawerNavigator } from '@react-navigation/drawer';
