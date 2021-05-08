3232c00559a47de581bacf342af7abbee61d523a
Renato Matta Machado <renatommps@gmail.com>
2021-05-08 20:43:19 -0300

    Add accidents filter by date

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,19 +1,72 @@
 import React, { useEffect, useState } from "react";
-import { Alert, ActivityIndicator, View, Text } from "react-native";
+import { Alert, ActivityIndicator, Modal, View, Text, TouchableHighlight } from "react-native";
 import { styles } from "./styles";
 import { UrlTile } from "react-native-maps";
-import { Heatmap, Marker, PROVIDER_DEFAULT } from "react-native-maps";
+import { Heatmap, Marker } from "react-native-maps";
 import MapView from "react-native-map-clustering";
 import { IconButton, Colors } from "react-native-paper";
 import * as Location from "expo-location";
 
+type Accident = {
+  data: string,
+  date: Date,
+  descrivao?: string,
+  detalhes?: string,
+  id: string,
+  latitude: string,
+  longitude: string
+};
+
+const MAX_ACCIDENTS_VALUE: number = 100;
+
 const Home = () => {
   const [trafficView, setTrafficView] = useState<boolean>(false);
-  const [location, setLocation] = useState<Location.LocationObject | null>(
-    null
-  );
-  const [acidentes, setAcidentes] = useState<any[]>([]);
+  const [location, setLocation] = useState<Location.LocationObject | null>(null);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [accidents, setAccidents] = useState<Accident[]>([]);
+  const [filteredAccidents, setFilteredAccidents] = useState<Accident[]>([]);
+  const [filterText, setFilterText] = useState<string>("all");
+  const [filterValue, setFilterValue] = useState<number | null>(null);
 
+  // This will filter the accidentes when the filter value change.
+  useEffect(() => {
+    (async () => {
+      let newFilteredAccidents: Accident[] = JSON.parse(JSON.stringify(accidents));
+
+      if (filterValue == null) {
+        // We allow at most 100 accidents (for performance reasons)
+        setFilteredAccidents(newFilteredAccidents);
+        return;
+      };
+
+      const endDate: Date = new Date();
+      endDate.setHours(0, 0, 0, 0);
+
+      newFilteredAccidents = accidents.filter((accident: Accident) => {
+        const startDate: Date | undefined = accident.date;
+
+        // If the accident date is not defined we filter the accident by default.
+        if (!startDate) return false;
+
+        // If the accident date is bigger than the current date we also filter it
+        // (this case should never happen, and is probably an invalid date).
+        if (startDate.getTime() > endDate.getTime()) return false;
+
+        // get total seconds between the times
+        const timeDiffInSeconds: number = ((endDate.getTime() - startDate.getTime()) / 1000);
+
+        // calculate number of whole days. OBS: 86400 is the amount of seconds in one day.
+        const daysPassed = Math.floor(timeDiffInSeconds / 86400);
+
+        return daysPassed <= filterValue;
+      });
+
+      // We allow at most 100 accidents (for performance reasons)
+      setFilteredAccidents(newFilteredAccidents);
+    })();
+  }, [filterValue]);
+
+  // Get permission to get user location.
   useEffect(() => {
     (async () => {
       const { status } = await Location.requestForegroundPermissionsAsync();
@@ -40,13 +93,38 @@ const Home = () => {
     })();
   }, []);
 
+  // Fetch accidentes from server.
   useEffect(() => {
     (async () => {
       try {
         fetch("https://cris.seplag.pe.gov.br/geolocations")
           .then((response) => response.json())
           .then((json) => {
-            if (Array.isArray(json)) setAcidentes(json.slice(100));
+            if (!Array.isArray(json)) return;
+
+            // Filter all accidentes that has no "data".
+            let accidents = json.filter(accident => accident.data != null);
+
+            // Add Date to all the accidents.
+            accidents.forEach((accident: Accident) => {
+              const stringDate: string | undefined = accident.data;
+
+              // convert string date to array of number.
+              const splitedDate: number[] = stringDate.split('/').map((date: string) => Number.parseInt(date));
+
+              // create a date passing on the constructor yyyy, mm - 1, dd.
+              const date: Date = new Date(splitedDate[2], splitedDate[1] - 1, splitedDate[0]);
+              date.setHours(0, 0, 0, 0);
+
+              // save the date on the accident object.
+              accident['date'] = date;
+            });
+
+            // Sorte the accidents descending by Date.
+            accidents = accidents.sort(sortAccidentsDescendentByDate);
+
+            setAccidents(accidents);
+            setFilteredAccidents(accidents.slice(0, MAX_ACCIDENTS_VALUE));
           })
           .catch((error) => {
             console.log("Errror getting accident location: " + error.message);
@@ -58,14 +136,30 @@ const Home = () => {
     })();
   }, []);
 
+  const sortAccidentsDescendentByDate = (a: Accident, b: Accident) => {
+    if (!b.date || !a.date) return 0;
+    return b.date.getTime() - a.date.getTime();
+  };
+
   const getLocations = () => {
-    return acidentes.map((acidente: any) => {
+    return filteredAccidents.map((acidente: any) => {
       return {
         latitude: acidente.latitude,
         longitude: acidente.longitude,
         weight: 1,
       };
     });
+  };
+
+  const showFilterModal = () => {
+    setModalVisible(true);
+    // console.log('filter modal called!');
+  };
+
+  const filterAccidents = (text: string, value: number | null) => {
+    console.log('filter accidentes called. text:', text, ', value:', value);
+    setFilterText(text);
+    setFilterValue(value);
   };
 
   return (
@@ -100,12 +194,12 @@ const Home = () => {
             maximumZ={19}
             flipY={false}
           />
-          {acidentes[0] != null && (
+          {filteredAccidents[0] != null && (
             <Heatmap points={getLocations()} radius={50} opacity={0.5} />
           )}
 
-          {acidentes[0] != null &&
-            acidentes.map((acidente: any) => {
+          {filteredAccidents[0] != null &&
+            filteredAccidents.map((acidente: any) => {
               return (
                 <Marker
                   key={acidente.id}
@@ -134,6 +228,66 @@ const Home = () => {
         size={50}
         onPress={() => setTrafficView(!trafficView)}
       />
+      <IconButton
+        icon={"filter"}
+        style={styles.filterIcon}
+        color={Colors.grey900}
+        size={40}
+        onPress={showFilterModal}
+      />
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => setModalVisible(false)}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Filtro de acidentes por período</Text>
+            <TouchableHighlight
+              style={(filterText == "all") ? styles.active : styles.inactive}
+              onPress={() => filterAccidents("all", null)}
+            >
+              <Text style={(filterText == "all") ? styles.modalTextActive : styles.modalTextInactive}>
+                Todos os acidentes
+              </Text>
+            </TouchableHighlight>
+            <TouchableHighlight
+              style={(filterText == "currentMonth") ? styles.active : styles.inactive}
+              onPress={() => filterAccidents("currentMonth", 30)}
+            >
+              <Text style={(filterText == "currentMonth") ? styles.modalTextActive : styles.modalTextInactive}>
+                Acidentes dos ultimos 30 dias
+              </Text>
+            </TouchableHighlight>
+            <TouchableHighlight
+              style={(filterText == "currentWeek") ? styles.active : styles.inactive}
+              onPress={() => filterAccidents("currentWeek", 7)}
+            >
+              <Text style={(filterText == "currentWeek") ? styles.modalTextActive : styles.modalTextInactive}>
+              Acidentes dos ultimos 7 dias
+              </Text>
+            </TouchableHighlight>
+            <TouchableHighlight
+              style={(filterText == "currentDay") ? styles.active : styles.inactive}
+              onPress={() => filterAccidents("currentDay", 1)}
+            >
+              <Text style={(filterText == "currentDay") ? styles.modalTextActive : styles.modalTextInactive}>
+              Acidentes do dia
+              </Text>
+            </TouchableHighlight>
+
+            <TouchableHighlight
+              style={styles.exitModalBtn}
+              onPress={() => setModalVisible(false)}
+            >
+              <Text style={styles.modalTextActive}>
+              Fechar Filtro
+              </Text>
+            </TouchableHighlight>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 };
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -38,4 +38,78 @@ export const styles = StyleSheet.create({
     bottom: 0,
     right: 0,
   },
+  filterIcon: {
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    borderWidth: 3,
+    borderColor: "black"
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center",
+    fontWeight: "bold"
+  },
+  openButton: {
+    backgroundColor: "#F194FF",
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  active: {
+    backgroundColor: "#EDC951",
+    borderRadius: 10,
+    marginBottom: 15,
+    paddingVertical: 4,
+    paddingHorizontal: 8
+  },
+  inactive: {
+    backgroundColor: "#ffffff",
+    borderRadius: 10,
+    borderColor: "#EDC951",
+    borderWidth: 2,
+    marginBottom: 15,
+    paddingVertical: 4,
+    paddingHorizontal: 8
+  },
+  modalTextActive: {
+    color: "#000000",
+    fontWeight: "bold"
+  },
+  modalTextInactive: {
+    color: "#000000",
+  },
+  exitModalBtn: {
+    marginTop: 10,
+    borderRadius: 10,
+    borderWidth: 2,
+    paddingVertical: 4,
+    paddingHorizontal: 8
+  }
 });
